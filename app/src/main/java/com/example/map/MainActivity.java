0a3207f8ffccffe457e7b3f2b1cec3f6bb57adf7
Frranky <srfranky228@gmail.com>
2021-12-22 12:45:44 +0700

    001-Start

---
+++ app/src/main/java/com/example/map/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.map;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.mapview.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mapview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MapKitFactory.setApiKey("52ff5c15-57b0-4dd0-b00d-5a6c50a6429b");
+        MapKitFactory.initialize(this);
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_main);
+        mapview = (MapView)findViewById(R.id.mapview);
+        mapview.getMap().move(
+                new CameraPosition(new Point(56.5, 85), 11.0f, 0.0f, 0.0f),
+                new Animation(Animation.Type.SMOOTH, 0),
+                null);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapview.onStop();
+        MapKitFactory.getInstance().onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapview.onStart();
+        MapKitFactory.getInstance().onStart();
+    }
+
+
+}
